830c1c6fb0b6e6302ed7790076df3dc8a126895e
Philip <philisandmann@gmail.com>
2022-12-06 13:28:23 +0100

    day 6 update

--- day06.py
+++ day06.py
@@ -2,23 +2,19 @@
 with open("inputs/day06.txt", "r") as f:
     input = f.read()
 
-solution1 = -1
-for (i, char) in enumerate(input):
-    if i >= 3:
-        s = set(input[i-4:i])
-        if(len(s) >= 4):
-            solution1 = i
-            break 
+for (i, char) in enumerate(input[3:], start = 3):
+    s = set(input[i-4:i])
+    if(len(s) >= 4):
+        solution1 = i
+        break 
 print(solution1)
 
 #part2 
-solution1 = -2
-for (i, char) in enumerate(input):
-    if i >= 13:
-        s = set(input[i-14:i])
-        if(len(s) >= 14):
-            solution2 = i
-            break 
+for (i, char) in enumerate(input[13:], start = 13):
+    s = set(input[i-14:i])
+    if(len(s) >= 14):
+        solution2 = i
+        break 
 print(solution2)
 
 
